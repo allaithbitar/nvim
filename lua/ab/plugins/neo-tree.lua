88e4397addb9b270fbe763469374a4796c06b47b
allaithbitar <allaithbitar.code0@gmail.com>
2025-02-12 10:49:09 +0300

    moved neo tree to right and always shawn hidden files

--- lua/ab/plugins/neo-tree.lua
+++ lua/ab/plugins/neo-tree.lua
@@ -7,10 +7,19 @@ return {
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+				},
+			},
+		})
+	end,
 	keys = {
 		{
 			"<leader>e",
-			"<cmd>Neotree filesystem reveal toggle left<CR>",
+			"<cmd>Neotree filesystem reveal toggle right<CR>",
 			desc = "Files Explorer",
 		},
 	},
